82c28c784af8ace75eaf95682c553e408d4e9216
Andrew Matz <andy@Andrews-MacBook-Pro.local>
2020-04-13 08:20:00 -0500

    Module 5 - Creating react Components

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Administration</h1>
+      <p>React, Fluc and React Router for Ultra-Responsive web apps</p>
+      <a href="/about">About Page</a>
+    </div>
+  );
+}
+
+export default HomePage;
